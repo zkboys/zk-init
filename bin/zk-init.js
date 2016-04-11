e71000d1ca7b7a404f04acae9f8dd168a7adad67
wangshubin <wangshubin@xiaoyage.com>
2016-04-12 02:39:54 +0800

    a

--- bin/zk-init.js
+++ bin/zk-init.js
@@ -1,48 +1,76 @@
 #!/usr/bin/env node
 var program = require('commander');
-var child_process = require('child_process');
-var rimraf = require('rimraf');
-var fs = require('fs');
-var ncp = require('ncp').ncp;
-var join = require('path').join;
 var vfs = require('vinyl-fs');
+var fs = require('fs');
+var through = require('through2');
+var path = require('path');
+var join = path.join;
+var basename = path.basename;
+
+// 这个文件下所有正常写得相对路相对的是 命令 执行路径
 program
     .version(require('../package').version)
     .usage('[options] <file ...>')
     .option('-t, --template-name <s>', 'The name of the template which you want to init')
     .option('-r, --rename <s>', 'Rename the template')
-    .option('-n, --no-folder', 'No folder')
+    .option('-f, --folder', 'Has folder')
     .parse(process.argv);
+var templateName = program.templateName;
+if (program.args && program.args.length == 1) {
+    templateName = program.args[0];
+}
 
-
-var templateNames = fs.readdirSync('../template');
+var templateNames = [
+    'jquery-simple',
+    'jquery-webpack'
+];
 var templateNameArr = templateNames.filter(function (name, index) {
-    return program.templateName === name;
+    return templateName === name;
 });
+
 if (!templateNameArr || !templateNameArr.length) {
-    console.log('No such template: %s', program.templateName);
+    console.log('No such template: %s', templateName);
     console.log('You can use:');
     for (var i = 0; i < templateNames.length; i++) {
         console.log(templateNames[i]);
     }
     return;
 } else {
-    var templateName = templateNameArr[0];
-    console.log('init %s...', templateName);
-    ncp(join(__dirname ,'../template/'+templateName), './'+templateName, function (err) {
-        if (err) {
-            return console.error(err);
-        }
-        console.log('done!');
-    });
-
+    //initTemplate(templateName);
 }
-/*
- console.log(' templateName: %s', program.templateName);
 
- console.log(' rename: %s', program.rename);
- console.log(' noFolder: %s', program.noFolder);
 
- console.log(' args: %s', program.args);
+function initTemplate(templateName) {
+    var cwd = join(__dirname, '../template', templateName);
+    var dest = process.cwd();//返回运行当前脚本的工作目录的路径。
+    if (program.folder) {
+        dest = join(process.cwd(), templateName);
+    }
+    if (program.rename) {
+        dest = join(process.cwd(), program.rename);
+    }
+    vfs.src('**/*', {cwd: cwd, cwdbase: true, dot: true})
+        .pipe(template(dest))
+        .pipe(vfs.dest(dest))
+        .on('end', function () {
+            //fs.renameSync(path.join(dest, 'gitignore'), path.join(dest, '.gitignore'));
+            //require('../lib/install');
+        })
+        .resume();
+}
 
- */
+function template(dest) {
+    return through.obj(function (file, enc, cb) {
+        if (!file.stat.isFile()) {
+            return cb();
+        }
+
+        console.log('Write %s', simplifyFilename(join(dest, basename(file.path))));
+        this.push(file);
+        cb();
+    });
+}
+
+function simplifyFilename(filename) {
+    return filename.replace(process.cwd(), ".");
+}
