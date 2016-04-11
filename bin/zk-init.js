b3e6076d398cbc3a0a8cc5bbe115091b916d50b8
wangshubin <wangshubin@xiaoyage.com>
2016-04-11 22:57:05 +0800

    写文档

--- bin/zk-init.js
+++ bin/zk-init.js
@@ -4,3 +4,10 @@ if (process.argv.length === 3 &&
     console.log(require('../package').version);
     return;
 }
+if (process.argv.length === 3 &&
+    (process.argv[2] === '-h' || process.argv[2] === '--help')) {
+    console.log('Help is not completed');
+    return;
+}
+console.log(process.argv);
+console.log('you can run zk-init -h to show  how to use this tool!');
