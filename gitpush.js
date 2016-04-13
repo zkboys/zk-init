5ebae77a7437f752e691abd94b10c376feb51083
wangshubin <wangshubin@xiaoyage.com>
2016-04-13 11:14:27 +0800

    测试

--- gitpush.js
+++ gitpush.js
@@ -1,19 +1,22 @@
 var exec = require('child_process').exec;
 
 exec("git add .", function (error, stdout, stderr) {
-
+    stdout && console.log(stdout);
+    stderr && console.log(stderr);
     if (error !== null) {
         console.log('git add error: ' + error);
         return;
     }
     exec("git commit -m '测试'", function (error, stdout, stderr) {
-
+        stdout && console.log(stdout);
+        stderr && console.log(stderr);
         if (error !== null) {
             console.log('git commit error: ' + error);
             return;
         }
         exec("git push origin master ", function (error, stdout, stderr) {
-
+            stdout && console.log(stdout);
+            stderr && console.log(stderr);
             if (error !== null) {
                 console.log('git push error: ' + error);
                 return;
